249ee97594838917e9478a4b8eb50de1cf91459e
Giuliana Martini <yulimartini94@gmail.com>
2024-11-09 14:08:51 -0300

    added checkbox, combobox, command, context-menu and menubar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,6 +15,11 @@ const links = [
   { name: "avatar", href: "avatar" },
   { name: "card", href: "card" },
   { name: "carousel", href: "carousel" },
+  { name: "checkbox", href: "checkbox" },
+  { name: "command", href: "command" },
+  { name: "combobox", href: "combobox" },
+  { name: "context menu", href: "context-menu" },
+  { name: "menu bar", href: "menubar" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
@@ -156,7 +161,7 @@ export default function DashboardLayout({
               </div>
             </div>
           </main>
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
